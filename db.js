bbc513c24cd8430df5ea666ea1db61397c4444bc
MarcolinoNVS <145811157+MarcolinoNVS@users.noreply.github.com>
2024-12-04 18:17:44 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,16 @@
-const mysql = require("mysql2");
+require('dotenv').config(); // Para carregar as variáveis do .env localmente
+const mysql = require('mysql2/promise'); // Utilize o driver mysql2 para suporte a Promises
 
+// Criação de conexão com o banco de dados
 const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT || 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // Porta padrão do MySQL
+  waitForConnections: true,
+  connectionLimit: 10, // Limite de conexões no pool
+  queueLimit: 0, // Sem limite na fila
 });
 
 module.exports = pool;
