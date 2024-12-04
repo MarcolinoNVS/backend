15b0e01a219e61c7acaebe61db37b428900d8e3f
MarcolinoNVS <145811157+MarcolinoNVS@users.noreply.github.com>
2024-12-04 19:57:05 -0300

    Update auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const express = require("express");
 const bcrypt = require("bcrypt");
-const db = require("../../db"); // Ajuste o caminho para o arquivo de conexão ao banco de dados
+const db = require("../db");
 const router = express.Router();
 
 const verifyToken = (req, res, next) => {
